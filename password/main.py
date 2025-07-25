63493096d2cfcf9206b3214ed21b3f41eac4287d
SMIT PATEL <smit.ppatel11@gmail.com>
2025-07-25 04:25:21 +0000

    Renamed

--- password/main.py
+++
@@ -1,9 +0,0 @@
-import random as rnd;
-
-char = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRESTUVWXYZ`~!@#$%^&()[]-_=+-/<>.,?";
-len = int(input("Enter Length : "));
-word = "";
-
-for w in range(len):
-    word = word + rnd.choice(char);
-print(word);
