33c5edda0c7e929b7a417f78c7d2a98e3efb9b35
SMIT PATEL <smit.ppatel11@gmail.com>
2025-07-25 04:22:23 +0000

    Dice-roller Completed

---
+++ dicel-roller/dice-roller.py
@@ -0,0 +1,12 @@
+import random as rnd;
+
+dNum = int(input("Number of Dice : "));
+sNum = int(input("Number of Side : "));
+
+try :
+    for n in range(int(dNum)) :
+        num = rnd.randint(1, int(sNum))
+        print(num);
+except :
+    print("Opps!");
+
