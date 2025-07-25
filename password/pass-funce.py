365cafde9abe41549463c7c287d2e0d22880ad69
SMIT PATEL <smit.ppatel11@gmail.com>
2025-07-25 04:10:02 +0000

    Password

---
+++ password/pass-funce.py
@@ -0,0 +1,10 @@
+import random as rnd;
+
+def func(char) :
+    len = int(input("Enter Length : "))
+    word = ""
+
+    for w in range(len) :
+        word = word + rnd.choice(char)
+    print(word);
+func("abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRESTUVWXYZ`~!@#$%^&()[]-_=+-/<>.,?");
