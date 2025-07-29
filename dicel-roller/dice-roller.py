288c5a7e9ae5e18e5394bf9c65df3908b7d4a2ac
SMIT PATEL <smit.ppatel11@gmail.com>
2025-07-29 15:45:34 +0530

    dice-roller.py

--- dicel-roller/dice-roller.py
+++ dicel-roller/dice-roller.py
@@ -3,10 +3,10 @@ import random as rnd;
 dNum = int(input("Number of Dice : "));
 sNum = int(input("Number of Side : "));
 
-try :
-    for n in range(int(dNum)) :
+try:
+    for n in range(int(dNum)):
         num = rnd.randint(1, int(sNum))
         print(num);
-except :
+except:
     print("Opps!");
 
